a39d49ec445b2fbeb9061507bc9e21f1bf59bb5a
amjadcp <amjadcp03@gmail.com>
2022-03-01 18:10:33 +0530

    tohost

--- home/models.py
+++ home/models.py
@@ -6,4 +6,4 @@ class Diary(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date = models.DateField()
     title = models.CharField(max_length=50)
-    body = models.TextField(max_length=1000)
+    body = models.TextField(max_length=10000)
