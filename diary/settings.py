f932027490a06e287999d4691340cac5679876cf
amjadcp <amjadcp03@gmail.com>
2022-03-01 16:49:10 +0530

    update:update requirements.txt

--- diary/settings.py
+++ diary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,7 +128,7 @@ STATICFILES_DIRS = [BASE_DIR, 'static',]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+django_heroku.settings(locals())
 # custom auth user model
 AUTH_USER_MODEL = 'users.User'
 
